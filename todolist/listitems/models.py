4a3b42a0fd0990f6e7868b0635c1cede33ab6eeb
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-06 11:24:38 -0400

    Quick edits

--- todolist/listitems/models.py
+++ todolist/listitems/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    task_text = models.CharField(max_length=200)
+    assignment_date = models.DateTimeField('date assigned')
+
+
+class Deadline(models.Model):
+    task = models.ForeignKey(Task, on_delete=models.CASCADE)
+    deadline_text = models.CharField(max_length=200)
+    due_date = models.DateTimeField('date due')
