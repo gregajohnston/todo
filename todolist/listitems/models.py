9c9c639361ffcea10ae94743a34d059801ce3fc5
Greg Johnston <greg.a.johnston@gmail.com>
2016-05-06 12:08:41 -0400

    Reduced models to tasks only

--- todolist/listitems/models.py
+++ todolist/listitems/models.py
@@ -3,10 +3,8 @@ from django.db import models
 
 class Task(models.Model):
     task_text = models.CharField(max_length=200)
+    task_importance = models.IntegerField(max_length=200)
+    task_status = models.CharField(max_length=200)
     assignment_date = models.DateTimeField('date assigned')
-
-
-class Deadline(models.Model):
-    task = models.ForeignKey(Task, on_delete=models.CASCADE)
-    deadline_text = models.CharField(max_length=200)
     due_date = models.DateTimeField('date due')
+    completion_date = models.DateTimeField('date completed')
